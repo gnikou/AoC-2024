cd6dd22fd1d02d33d2188e609ee5b5cf14847944
gnikou <georgenikou@yahoo.gr>
2024-12-08 16:20:08 +0200

    Day 3 Part 2

--- day3/day3.py
+++ day3/day3.py
@@ -6,7 +6,29 @@ with open(file) as f:
     instructions = f.read()
 
 # print(instructions)
-result = re.findall('mul\((\d+){1,3},(\d+){1,3}\)', instructions)
-s = sum([(int(x) * int(y)) for x, y in result])
+matches = re.findall('mul\((\d+){1,3},(\d+){1,3}\)', instructions)
+result = sum([(int(x) * int(y)) for x, y in matches])
 
-print(s)
+print(result)
+
+# part 2
+result = 0
+flag = 1
+mul_list = []
+
+matches = re.findall('(mul\((?:\d+){1,3},(?:\d+){1,3}\))|(do(?:n\'t)?\(\))', instructions)
+for match in matches:
+    if len(match[0]):
+        if flag:
+            mul_list.append(match[0])
+    else:
+        if match[1] == 'don\'t()':
+            flag = 0
+        else:
+            flag = 1
+
+for instructions in mul_list:
+    match = re.findall('mul\((\d+){1,3},(\d+){1,3}\)', instructions)
+    result += sum([(int(x) * int(y)) for x, y in match])
+
+print(result)
